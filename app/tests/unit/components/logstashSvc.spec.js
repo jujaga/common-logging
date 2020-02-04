b54205e47784ba1de1e74f56042a29186636e287
Jeremy Ho <jujaga@gmail.com>
2020-02-04 11:31:48 -0800

    Add logstash.log test coverage
    Signed-off-by: Jeremy Ho <jujaga@gmail.com>

--- app/tests/unit/components/logstashSvc.spec.js
+++ app/tests/unit/components/logstashSvc.spec.js
@@ -1,3 +1,5 @@
+const axios = require('axios');
+const config = require('config');
 const moment = require('moment');
 const Problem = require('api-problem');
 
@@ -21,24 +23,80 @@ describe('logMany', () => {
     logSpy.mockReset();
   });
 
-  it('should return nothing on success', () => {
-    logSpy.mockReturnValue(undefined);
+  afterAll(() => {
+    logSpy.mockRestore();
+  });
 
-    expect(logstashSvc.logMany([clogs])).resolves.toBeUndefined();
+  it('should return nothing on success', () => {
+    logSpy.mockResolvedValue(undefined);
+    const result = logstashSvc.logMany([clogs]);
+
+    expect(result).resolves.toBeUndefined();
     expect(logSpy).toHaveBeenCalledTimes(1);
     expect(logSpy).toHaveBeenCalledWith(clogs);
   });
 
-  it('should return an array of CLOGS objects', async () => {
+  it('should return an array of CLOGS objects', () => {
     const problem = new Problem(500);
     logSpy.mockImplementation(() => { throw problem; });
+    const result = logstashSvc.logMany([clogs]);
 
-    expect(logstashSvc.logMany([clogs])).rejects.toThrow(problem);
+    expect(result).rejects.toThrow(problem);
     expect(logSpy).toHaveBeenCalledTimes(1);
     expect(logSpy).toHaveBeenCalledWith(clogs);
   });
 });
 
 describe('log', () => {
+  const axiosSpy = jest.spyOn(axios, 'post');
+  const axiosOptions = {
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    maxBodyLength: Infinity,
+    maxContentLength: Infinity
+  };
+  const url = config.get('elkStack.logstashUrl');
 
+  afterEach(() => {
+    axiosSpy.mockReset();
+  });
+
+  afterAll(() => {
+    axiosSpy.mockRestore();
+  });
+
+  it('should return nothing on success', () => {
+    axiosSpy.mockResolvedValue({ status: 200 });
+    const result = logstashSvc.log(clogs);
+
+    expect(result).resolves.toBeUndefined();
+    expect(axiosSpy).toHaveBeenCalledTimes(1);
+    expect(axiosSpy).toHaveBeenCalledWith(url, clogs, axiosOptions);
+  });
+
+  it('should throw on a non 200 response', () => {
+    axiosSpy.mockResolvedValue({ status: 201 });
+    const result = logstashSvc.log(clogs);
+
+    expect(result).rejects.toThrow(new Problem(500, 'Unknown LOGSTASH Error'));
+    expect(axiosSpy).toHaveBeenCalledTimes(1);
+    expect(axiosSpy).toHaveBeenCalledWith(url, clogs, axiosOptions);
+  });
+
+  it('should throw on an error response with data', () => {
+    const status = 500;
+    axiosSpy.mockImplementation(() => {
+      throw {
+        response: {
+          data: {}, status: status
+        },
+      };
+    });
+    const result = logstashSvc.log(clogs);
+
+    expect(result).rejects.toThrow(new Problem(status));
+    expect(axiosSpy).toHaveBeenCalledTimes(1);
+    expect(axiosSpy).toHaveBeenCalledWith(url, clogs, axiosOptions);
+  });
 });
